fddf2a2e39c2990a1ca962a7b22ae85af9421c4b
VigM <vmikulis@figure.com>
2022-11-03 13:37:34 -0600

    version up to wcjs2.0.2 no more custom webpack

--- extra-webpack-config.js
+++ extra-webpack-config.js
@@ -1,41 +1,21 @@
-const webpack = require('webpack');
+const webpack = require("webpack");
 
 module.exports = {
-	plugins: [
-		// Work around for Buffer is undefined:
-		// https://github.com/webpack/changelog-v5/issues/10
-		new webpack.ProvidePlugin({
-			Buffer: ['buffer', 'Buffer'],
-		}),
-		new webpack.ProvidePlugin({
-			process: 'process/browser',
-		}),
-	],
-	module: {
-		rules: [
-			{
-				test: /\.(png|jpe?g|gif|svg)$/i,
-				use: [
-					{
-						loader: 'file-loader',
-					},
-				],
-			},
-		],
-	},
-	resolve: {
-		extensions: ['.ts', '.js'],
-		fallback: {
-			crypto: require.resolve('crypto-browserify'),
-			buffer: require.resolve("buffer"),
-			util: require.resolve("util"),
-			stream: require.resolve("stream-browserify"),
-			process: require.resolve("process")
-		},
-	},
-	node: {
-		global: true,
-		__filename: true,
-		__dirname: true,
-	}
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
+    ],
+  },
+  node: {
+    global: true,
+    __filename: true,
+    __dirname: true,
+  },
 };
